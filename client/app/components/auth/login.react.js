9d8b4bf6cfca7e432ed000b11b7a738b1a1466dd
prithvi_anantharaj <prithvi.anantharaj@gmail.com>
2016-12-14 19:12:16 -0800

    Updated Login and Signup pages.

--- client/app/components/auth/login.react.js
+++ client/app/components/auth/login.react.js
@@ -1,35 +1,17 @@
 import React from 'react';
-import TextField from 'material-ui/TextField';
-import { RadioButton, RadioButtonGroup } from 'material-ui/RadioButton';
-import Checkbox from 'material-ui/Checkbox';
-import SelectField from 'material-ui/SelectField';
-import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
 
 export default class Login extends React.Component {
   constructor(props) {
     super(props);
   }
 
-  function getInitialState() {
-    return {
-        emailValue: '',
-        passwordValue: ''
-    }
-  }
-
-  function handleTextFieldChange(e) {
-    this.setState({
-      emailValue: e.target.value,
-      passwordValue: e.target.value
-    });
-  }
 
   render() {
     console.log('Log In');
     return (
       <div>
-          <TextField value={this.state.emailValue} onChange={this._handleTextFieldChange} />
-          <TextField value={this.state.passwordValue} onChange={this._handleTextFieldChange} />
+        <RaisedButton label="Log In" />
       </div>
     );
   }
