79b1c634c12c367bdb3860981958f19350335aed
Andrew Cookro <andrew.cookro@gmail.com>
2016-12-17 10:28:49 -0800

    Create theme modules for each team member to be filled out later. Can now access each by team member name.

--- client/app/components/theme/theme.js
+++ client/app/components/theme/theme.js
@@ -4,7 +4,8 @@ import * as Colors from 'material-ui/styles/colors';
 import Spacing from 'material-ui/styles/spacing';
 import zIndex from 'material-ui/styles/zIndex';
 
-export default {
+// Prithvi Winterfresh
+module.exports.prithvi = {
   zIndex: zIndex,
   spacing: Spacing,
   fontFamily: 'Roboto, sans-serif',
@@ -25,3 +26,75 @@ export default {
     ToolbarGroup: Colors.Blue200,
   }
 };
+
+// Brittany
+module.exports.brittany = {
+  zIndex: zIndex,
+  spacing: Spacing,
+  fontFamily: 'Roboto, sans-serif',
+  AppBar: {
+    textColor: Colors.black
+  },
+  palette: {
+    primary1Color: Colors.Blue500,
+    primary2Color: Colors.Blue200,
+    primary3Color: Colors.Blue300,
+    accent1Color: Colors.Blue300,
+    accent2Color: Colors.Blue300,
+    accent3Color: Colors.Blue300,
+    textColor: Colors.teal900,
+    alternateTextColor: Colors.white,
+    canvasColor: Colors.white,
+    pickerHeaderColor: Colors.Blue200,
+    ToolbarGroup: Colors.Blue200,
+  }
+};
+
+// Calvin
+module.exports.calvin = {
+  zIndex: zIndex,
+  spacing: Spacing,
+  fontFamily: 'Roboto, sans-serif',
+  AppBar: {
+    textColor: Colors.black
+  },
+  palette: {
+    primary1Color: Colors.Blue500,
+    primary2Color: Colors.Blue200,
+    primary3Color: Colors.Blue300,
+    accent1Color: Colors.Blue300,
+    accent2Color: Colors.Blue300,
+    accent3Color: Colors.Blue300,
+    textColor: Colors.teal900,
+    alternateTextColor: Colors.white,
+    canvasColor: Colors.white,
+    pickerHeaderColor: Colors.Blue200,
+    ToolbarGroup: Colors.Blue200,
+  }
+};
+
+// Andrew
+module.exports.andrew = {
+  zIndex: zIndex,
+  spacing: Spacing,
+  fontFamily: 'Roboto, sans-serif',
+  AppBar: {
+    textColor: Colors.black,
+    //alternateTextColor: Colors.black,
+  },
+  palette: {
+    primary1Color: '#455A64',
+    primary2Color: '#607D8B',
+    primary3Color: '#757575',
+    accent1Color: Colors.Blue300,
+    accent2Color: Colors.Blue300,
+    accent3Color: Colors.Blue300,
+    textColor: '#212121',
+    alternateTextColor: Colors.white,
+    canvasColor: '#BDBDBD',
+    pickerHeaderColor: Colors.Blue200,
+    ToolbarGroup: Colors.Blue200,
+  }
+};
+
+
