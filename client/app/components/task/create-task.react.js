b041cabb5b6cd48d65eaefcdf182c5ddcb62f256
Brittany Starr <lunarunar@gmail.com>
2016-12-17 20:38:52 -0700

    added theme, noted change to db config for Tasks

--- client/app/components/task/create-task.react.js
+++ client/app/components/task/create-task.react.js
@@ -5,13 +5,57 @@ import Divider from 'material-ui/Divider';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import AddCircleOutline from 'material-ui/svg-icons/content/add-circle-outline';
+import RoutineStore from '../../flux/stores/routine-store';
+import TaskStore from '../../flux/stores/task-store';
+import TaskActions from '../../flux/actions/task-actions';
+import axios from 'axios';
 
 export default class CreateTask extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      name: '',
+      description: '',
+      completed: false,
+      RoutineId: RoutineStore.data.currentRoutine + 1
+      //added plus one due to issue with querying database - it returns the previous routine rather than the current one
     };
+  }
+
+  componentDidMount() {
+    this.getRoutineId()
+  }
+
+  getRoutineId() {
+    axios.get('/routines')
+      .then(function(routines){
+        console.log("ROUTINES FROM GET", routines)
+        var max = -1; //start at -1, to account for the first item in the database
+        routines.data.forEach(function(val){
+          if (val.id > max) {
+            max = val.id
+            console.log('currently at max', max, val.id)
+          }
+        })
+      RoutineStore.data.currentRoutine = max;
+      console.log("max ID in query", RoutineStore.data.currentRoutine)
+      });
+  }
+
+  handleChange(fieldName, event) {
+    this.setState({
+      [fieldName]: event.target.value
+    });
+  }
+
+  handleSubmit() {
+    TaskActions
+      .add({
+        name: this.state.name,
+        description: this.state.description,
+        completed: this.state.completed,
+        RoutineId: this.state.RoutineId
+      })
   }
 
   render() {
@@ -42,13 +86,16 @@ export default class CreateTask extends React.Component {
                   hintText="ex. Morning jog for 30 minutes"
                   floatingLabelText="Please Input Task Name"
                   fullWidth={true}
+                  onChange={this.handleChange.bind(this, 'name')}
                 /><br />
                 <TextField
                   hintText="ex. My morning jog consisted of 4 minute intervals with 1 minute rest periods along the San Fransisco Bay"
                   floatingLabelText="Please Input Task Description"
                   fullWidth={true}
                   multiLine={true}
-                  rows={4} />
+                  rows={4}
+                  onChange={this.handleChange.bind(this, 'description')}
+                />
                 <div><br />
                 <RaisedButton
                   label="Add Task"
@@ -56,6 +103,7 @@ export default class CreateTask extends React.Component {
                   primary={true}
                   icon={<AddCircleOutline />}
                   style={{marginLeft: '32%'}}
+                  onClick={this.handleSubmit.bind(this)}
                 />
               </div>
               </div>
